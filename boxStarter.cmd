2e4d022541b7efc2d48211f46dfc8161a27a6f31
Adrian Farnell <adrianjfarnell@gmail.com>
2019-07-20 23:24:56 +0100

    new 1903 install

---
+++ boxStarter.cmd
@@ -0,0 +1,5 @@
+start http://boxstarter.org/package/nr/url?https://gist.githubusercontent.com/r3adm3/87490781a7c1c85d1170/raw/237fc259ee6cab56865148afd27cfb8971f5f7a3/myBoxStarter.txt
+start powercfg -setdcvalueindex SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 FBD9AA66-9553-4097-BA44-ED6E9D65EAB8 0
+start powercfg -setacvalueindex SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 FBD9AA66-9553-4097-BA44-ED6E9D65EAB8 0
+start powercfg -SetActive SCHEME_CURRENT
+
